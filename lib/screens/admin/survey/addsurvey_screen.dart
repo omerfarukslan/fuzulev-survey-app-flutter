25ec21836d942b8912eefcccdc42e2bc0553c35d
Ömer Faruk <omer8aslanfaruk@outlook.com>
2025-09-02 11:29:02 +0300

    Revert "büyük Ş"
    This reverts commit bd2c4c93cd6aa5bb081163aaf51b8e87ec606714.

--- lib/screens/admin/survey/addsurvey_screen.dart
+++ lib/screens/admin/survey/addsurvey_screen.dart
@@ -45,7 +45,7 @@ class _AddSurveyScreenState extends State<AddSurveyScreen> {
         builder:
             (_) => CupertinoAlertDialog(
               title: Text("Uyarı"),
-              content: Text("Lütfen anket baŞlığını girin."),
+              content: Text("Lütfen anket başlığını girin."),
               actions: [
                 CupertinoDialogAction(
                   child: CupertinoButton(
